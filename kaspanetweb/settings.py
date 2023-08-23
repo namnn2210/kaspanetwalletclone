a7f9dfc18669c66373df01c349b601b2a6ab590d
Ngo Ngoc Nam <namngocngo22@gmail.com>
2023-08-23 10:00:52 +0700

    edit

--- kaspanetweb/settings.py
+++ kaspanetweb/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1y$xh7ky#01vo6xr71@zc#)p9q&daemc!z0e3!au!k!i9!)a$n'
+SECRET_KEY = '!@Ngocnam2210'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
